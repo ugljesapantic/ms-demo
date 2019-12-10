87fd81dc79ceb433113758415cf2d2588a72d8dd
Ugljesa Pantic <ugljesapantic@gmail.com>
2019-12-10 21:46:22 +0100

    post delete

--- src/pages/post-details/PostDetails.js
+++ src/pages/post-details/PostDetails.js
@@ -6,16 +6,26 @@ import styled from 'styled-components';
 import { LimitedWidthContainer } from '../../styles/utils';
 import Button from '../../components/Button';
 import { FeedContext, fbAuth, MyPostsContext } from '../../App';
-import { createComposideChatKey } from '../../utils/misc';
+import { createComposideChatKey, showSuccessToast } from '../../utils/misc';
 import { Loading } from '../../components/Loading';
+import { useIntl } from 'react-intl';
 
 const PostDetailsContainer = styled(LimitedWidthContainer)`
   padding-top: 2rem;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Actions = styled.div`
   display: flex;
 `;
+
+const StyledButton = styled(Button)`
+    margin: 1rem 0;
+
+    /* TODO I can do better than this */
+    ${({right}) => right ? 'margin-left: auto' : 'margin-right: auto'}
+`
 
 export const PostDetails = (props) => {
     let { id } = useParams();
@@ -24,6 +34,7 @@ export const PostDetails = (props) => {
     const hist = useHistory();
     const feedContext = useContext(FeedContext);
     const myPostsContext = useContext(MyPostsContext);
+    const intl = useIntl();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,10 +48,13 @@ export const PostDetails = (props) => {
     const selectedTags = feedContext.alive && feedContext.filters.tags ? feedContext.filters.tags.map(tag => tag.value) : [];
 
     const onDelete = async () => {
+        setLoading(true);
         await http('deletePost', 'DELETE', {id});
         myPostsContext.set(c => ({
             posts: c.posts.filter(post => post._id !== id)
         }))
+        showSuccessToast(intl.formatMessage({id: 'profile.post.delete'}))
+        setLoading(false);
         hist.push('/profile');
     }
 
@@ -56,11 +70,11 @@ export const PostDetails = (props) => {
     // TODO Loading screen high order component
     return (
         <PostDetailsContainer>
-            <Button style={{marginBottom: '20px'}} secondary text="Go back" onClick={() => hist.goBack()}/>
+            {!loading && <StyledButton  secondary text="Go back" onClick={() => hist.goBack()}/>}
             {loading ? <Loading /> : <Post selectedTags={selectedTags} post={post} details/>} 
             {!loading && <Actions>
-                {isMine && <Button style={{marginTop: '20px'}} negtative text="Delete" onClick={onDelete}/>}
-                {!isMine && <Button style={{marginTop: '20px', marginLeft: 'auto'}} primary text="Send message" onClick={sendMessage}/>}
+                {isMine && <StyledButton right negative text="Delete" onClick={onDelete}/>}
+                {!isMine && <StyledButton right primary text="Send message" onClick={sendMessage}/>}
             </Actions>}
         </PostDetailsContainer>
     )
